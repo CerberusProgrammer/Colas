44aa2dce75e78c04d5c1b301ffefa15a113a992a
Omar Flores Salazar <cerberusprogrammer@gmail.com>
2021-09-05 18:39:29 -0700

    arre

--- src/ColaCircular.java
+++ src/ColaCircular.java
@@ -1,6 +1,73 @@
-public class ColaCircular {
+public class ColaCircular<T> {
 
-    int maximo;
-    int inicio;
-    int[] cola;
+    private final int max;
+    private final T[] cola;
+    private int inicio;
+    private int fin;
+
+    public ColaCircular() {
+        this.max = 100;
+        this.inicio = -1;
+        this.fin = -1;
+        this.cola = (T[]) new Object[this.max +1];
+    }
+
+    public ColaCircular(int max) {
+        this.max = max;
+        this.inicio = -1;
+        this.fin = -1;
+        this.cola = (T[]) new Object[this.max +1];
+    }
+
+    public void insertar(T dato) {
+        if((fin == max-1 && inicio==0) || (fin + 1 == inicio))
+            return;
+        if(fin == max - 1 && inicio != 0)
+            fin = 0;
+        else
+            fin++;
+
+        cola[fin] = dato;
+
+        if(inicio == -1)
+            inicio = 0;
+    }
+
+    public T eliminar() {
+        T dato = null;
+
+        if(inicio ==- 1)
+            return null;
+
+        dato = cola[inicio];
+
+        if(inicio == fin) {
+            inicio=-1;
+            fin=-1;
+        }
+        if(inicio == max)
+            inicio=0;
+        else
+            inicio++;
+        return dato;
+    }
+
+    public String mostrar() {
+        int i = 0;
+        StringBuilder s = new StringBuilder();
+
+        if (inicio == -1)
+            return "";
+        else {
+            i = inicio;
+            do {
+                s.append(" ").append(cola[i]);
+                i++;
+                if (i == max && inicio > fin)
+                    i = 0;
+            } while (i != fin + 1);
+        }
+        return s.toString();
+    }
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
+import java.util.AbstractQueue;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class Main {
     public static void main(String[] args) {
+        /*
         ColaSImple<String> colaSImple = new ColaSImple<>();
         colaSImple.insertar("a");
         colaSImple.insertar("b");
@@ -7,5 +12,54 @@ public class Main {
         System.out.println(colaSImple.peek());
         colaSImple.eliminar();
         System.out.println(colaSImple.peek());
+
+        ColaCircular<String> colaCircular = new ColaCircular<>();
+        colaCircular.insertar("a");
+        colaCircular.insertar("b");
+        colaCircular.insertar("c");
+        colaCircular.insertar("d");
+        System.out.println(colaCircular.mostrar());
+        colaCircular.eliminar();
+        colaCircular.eliminar();
+        System.out.println(colaCircular.mostrar());
+         */
+
+        AbstractQueue<String> abstractQueue = new AbstractQueue<String>() {
+            @Override
+            public Iterator<String> iterator() {
+                return null;
+            }
+
+            @Override
+            public int size() {
+                return 0;
+            }
+
+            @Override
+            public boolean offer(String s) {
+                return false;
+            }
+
+            @Override
+            public String poll() {
+                return null;
+            }
+
+            @Override
+            public String peek() {
+                return null;
+            }
+        };
+
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        priorityQueue.add(1);
+        priorityQueue.add(3);
+        priorityQueue.add(2);
+        priorityQueue.add(0);
+        priorityQueue.poll();
+        System.out.println(priorityQueue.peek());
     }
 }
+
+
+
