53fcbcda1524b547a053293c966c2236fb52316e
Omar Flores Salazar <cerberusprogrammer@gmail.com>
2021-09-19 14:29:04 -0700

    Arreglado

--- src/Cola.java
+++ src/Cola.java
@@ -1,35 +1,28 @@
-public class ColaSImple<T> {
+public class Cola<T> {
+
     private int max;
     private T[] cola;
     private int inicio;
     private int fin;
 
-    public ColaSImple() {
+    public Cola() {
         this.max = 100;
-        this.inicio = 0;
-        this.fin = 0;
+        this.inicio = -1;
+        this.fin = -1;
         this.cola = (T[]) new Object[this.max +1];
     }
 
-    public ColaSImple(int max) {
+    public Cola(int max) {
         this.max = max;
-        this.inicio = 0;
-        this.fin = 0;
+        this.inicio = -1;
+        this.fin = -1;
         this.cola = (T[]) new Object[this.max +1];
-    }
-
-    public boolean colaLlena() {
-        return this.fin == this.max;
-    }
-
-    public boolean colaVacia() {
-        return this.inicio == this.fin;
     }
 
     public void insertar(T data) {
         if (fin < max) {
-            cola[fin] = data;
             fin++;
+            cola[fin] = data;
 
             if (fin == 0) {
                 inicio = 0;
@@ -56,5 +49,23 @@ public class ColaSImple<T> {
 
     public T peek() {
         return cola[inicio];
+    }
+
+    public String toString() {
+        int i = 0;
+        StringBuilder s = new StringBuilder();
+
+        if (inicio == -1)
+            return "";
+        else {
+            i = inicio;
+            do {
+                s.append(cola[i]).append(" ");
+                i++;
+                if (i == max && inicio > fin)
+                    i = 0;
+            } while (i != fin + 1);
+        }
+        return s.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,63 +1,14 @@
-import java.util.AbstractQueue;
-import java.util.Iterator;
-import java.util.PriorityQueue;
-
 public class Main {
     public static void main(String[] args) {
-        /*
-        ColaSImple<String> colaSImple = new ColaSImple<>();
-        colaSImple.insertar("a");
-        colaSImple.insertar("b");
-        colaSImple.insertar("c");
-        System.out.println(colaSImple.peek());
-        colaSImple.eliminar();
-        System.out.println(colaSImple.peek());
+        Cola<String> cola = new Cola<>();
+        cola.insertar("a");
+        cola.insertar("b");
+        cola.insertar("c");
+        System.out.println(cola.peek());
+        cola.eliminar();
+        System.out.println(cola.peek());
 
-        ColaCircular<String> colaCircular = new ColaCircular<>();
-        colaCircular.insertar("a");
-        colaCircular.insertar("b");
-        colaCircular.insertar("c");
-        colaCircular.insertar("d");
-        System.out.println(colaCircular.mostrar());
-        colaCircular.eliminar();
-        colaCircular.eliminar();
-        System.out.println(colaCircular.mostrar());
-         */
-
-        AbstractQueue<String> abstractQueue = new AbstractQueue<String>() {
-            @Override
-            public Iterator<String> iterator() {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean offer(String s) {
-                return false;
-            }
-
-            @Override
-            public String poll() {
-                return null;
-            }
-
-            @Override
-            public String peek() {
-                return null;
-            }
-        };
-
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
-        priorityQueue.add(1);
-        priorityQueue.add(3);
-        priorityQueue.add(2);
-        priorityQueue.add(0);
-        priorityQueue.poll();
-        System.out.println(priorityQueue.peek());
+        System.out.println(cola.toString());
     }
 }
 
